d5fc851fdd19a389a00ebdd2296d75cab908929a
Lauri Junkkari <latenssi@gmail.com>
2017-04-19 19:58:00 +0300

    Update default settings

--- settings-local.sh
+++ settings-local.sh
@@ -1,5 +1,6 @@
 export JAVACMD="java"
-export MIN_RAM="3G"
-export MAX_RAM="3G"
+export MIN_RAM="2G"
+export MAX_RAM="4G"
 export PERMGEN_SIZE="" # Ignored in Java 8
-export JAVA_PARAMETERS="-XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch"
+#export JAVA_PARAMETERS="-XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=50 -XX:+AggressiveOpts -XX:+AlwaysPreTouch"
+export JAVA_PARAMETERS="-XX:+UseParNewGC -XX:+UseConcMarkSweepGC"
